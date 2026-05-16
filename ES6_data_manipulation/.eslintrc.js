780b310d844747a0114a6d5418b23f1161be3048
your0T0 <yara241020@gmail.com>
2026-05-16 02:02:24 +0200

    adding

---
+++ ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+};
---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of map) {
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
+  }
+
+  return map;
+}
